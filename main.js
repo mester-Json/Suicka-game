6cf2f8b93c4f2e7007da853585307f7b76e6aea3
root <root@asusjayson.home>
2023-10-31 13:55:53 +0100

    maj

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-import { Engine, Render, Runner, Bodies, World, Body, Sleeping, Events } from "matter-js";
-import { Fruits } from "/fruit.js";
+import {
+  Engine,
+  Render,
+  Runner,
+  Bodies,
+  World,
+  Body,
+  Sleeping,
+  Events,
+} from "matter-js";
+import { Fruits } from "./fruit";
 
 const engine = Engine.create();
 const render = Render.create({
@@ -7,66 +16,67 @@ const render = Render.create({
   element: document.getElementById("matter-canvas"),
   options: {
     wireframes: false,
-    background: "url(./public/fond.jpg) no-repeat center center fixed ",
-    backgroundSize: "cover",
-    width: 620,
+    background: "#F7F4C8",
+    width: 700,
     height: 850,
   },
 });
 
 const world = engine.world;
-const ground = Bodies.rectangle(310, 840, 620, 30, {
+
+const ground = Bodies.rectangle(310, 820, 800, 60, {
   isStatic: true,
   render: {
-    fillStyle: 'black',
+    fillStyle: "#FF88A9",
   },
 });
 
-const leftwall = Bodies.rectangle(15, 395, 30, 860, {
+const leftWall = Bodies.rectangle(15, 395, 30, 790, {
   isStatic: true,
   render: {
-    fillStyle: 'black',
+    fillStyle: "#FF88A9",
   },
 });
 
-const rightwall = Bodies.rectangle(605, 395, 30, 860, {
+const rightWall = Bodies.rectangle(685, 395, 30, 790, {
   isStatic: true,
   render: {
-    fillStyle: 'black',
+    fillStyle: "#FF88A9",
   },
 });
 
-const topLigne = Bodies.rectangle(310, 50, 620, 2, {
+const topLine = Bodies.rectangle(310, 100, 720, 2, {
   isStatic: true,
   isSensor: true,
-  render: {
-    fillStyle: 'black',
-    label: top,
-  },
+  render: { fillStyle: "#FF88A9" },
+  label: "topLine",
 });
 
-World.add(world, [ground, leftwall, rightwall, topLigne]);
+World.add(world, [ground, leftWall, rightWall, topLine]);
 
-const runner = Runner.create();
-Runner.run(runner, engine)
 Render.run(render);
+Runner.run(engine);
 
-let score = 0;
 let currentBody = null;
 let currentFruit = null;
 let interval = null;
 let disableAction = false;
+let score = 0;
+
+
+
 
 function addCurrentFruit() {
   const randomFruit = getRandomFruit();
 
-  const body = Bodies.circle(200, 50, randomFruit.radius, {
+  const body = Bodies.circle(300, 50, randomFruit.radius, {
     label: randomFruit.label,
     isSleeping: true,
     render: {
       fillStyle: randomFruit.color,
+      //  sprite: { texture: `/${randomFruit.label}.svg` },
     },
-    restitution: 0.3,
+    restitution: 0.2,
   });
 
   currentBody = body;
@@ -79,40 +89,43 @@ function getRandomFruit() {
   const randomIndex = Math.floor(Math.random() * 5);
   const fruit = Fruits[randomIndex];
 
-  if (currentFruit && currentFruit.label == fruit.label)
+  if (currentFruit && currentFruit.label === fruit.label)
     return getRandomFruit();
 
   return fruit;
 }
 
-
-
 window.onkeydown = (event) => {
   if (disableAction) return;
+
   switch (event.code) {
     case "ArrowLeft":
       if (interval) return;
       interval = setInterval(() => {
-        if (currentBody.position.x - 20 > 30)
-          Body.setPosition(currentBody, { x: currentBody.position.x - 1, y: currentBody.position.y });
-      }, 5);
+        if (currentBody.position.x - 20 > 25)
+          Body.setPosition(currentBody, {
+            x: currentBody.position.x - 1,
+            y: currentBody.position.y,
+          });
+      }, 1);
       break;
     case "ArrowRight":
       if (interval) return;
       interval = setInterval(() => {
-        if (currentBody.position.x + 20 < 590)
-          Body.setPosition(currentBody, { x: currentBody.position.x + 1, y: currentBody.position.y });
-      }, 5);
+        if (currentBody.position.x + 20 < 650)
+          Body.setPosition(currentBody, {
+            x: currentBody.position.x + 1,
+            y: currentBody.position.y,
+          });
+      }, 1);
       break;
     case "Space":
-      if (disableAction) return;
       disableAction = true;
       Sleeping.set(currentBody, false);
       setTimeout(() => {
         addCurrentFruit();
         disableAction = false;
       }, 1000);
-      break;
   }
 };
 
@@ -122,20 +135,17 @@ window.onkeyup = (event) => {
     case "ArrowRight":
       clearInterval(interval);
       interval = null;
-      break;
   }
 };
 
 Events.on(engine, "collisionStart", (event) => {
-  event.pairs.forEach(collision => {
+  event.pairs.forEach((collision) => {
     if (collision.bodyA.label === collision.bodyB.label) {
-      World.remove(world, [collision.bodyA, collision.bodyB])
-
+      World.remove(world, [collision.bodyA, collision.bodyB]);
       const index = Fruits.findIndex(
-        (fruit => fruit.label === collision.bodyA.label)
+        (fruit) => fruit.label === collision.bodyA.label
       );
-
-      if (index == Fruits.length - 1) return;
+      if (index === Fruits.length - 1) return;
 
       const newFruit = Fruits[index + 1];
       const body = Bodies.circle(
@@ -145,22 +155,26 @@ Events.on(engine, "collisionStart", (event) => {
         {
           render: {
             fillStyle: newFruit.color,
+            // sprite: { texture: `/${newFruit.label}.svg` },
           },
           label: newFruit.label,
         }
       );
       World.add(world, body);
-      score += 10;
+      score += 25;
       updateScore();
-
     }
-    if ((collision.bodyA.label == "top" || collision.bodyB.label == "top") && !disableAction) {
-      {
-        alert("Game Over ");
-      }
+    if (
+      (collision.bodyA.label === "topLine" ||
+        collision.bodyB.label === "topLine") &&
+      !disableAction
+    ) {
+      alert("Game over");
     }
-  })
+  });
 });
+
+
 
 function updateScore() {
   const scoreElement = document.getElementById("score");
